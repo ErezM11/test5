d3bcb808c0793205768fed8c192abb32a09023f7
Davidmalo <dudi9821@gmail.com>
2025-03-23 15:56:33 +0200

    update navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,12 +2,17 @@ import Dropdown from 'react-bootstrap/Dropdown';
 export default function NavBar() {
   return (
     <div>
-         <div className="accounticon">
+        <div className="accounticon">
         <Dropdown>
         <Dropdown.Toggle variant="" id="dropdown-basic">
         <img src="src/assets/Untitled.png" alt="test" className="iconimage" />
       </Dropdown.Toggle>
-            </Dropdown>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Manage Account</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Payment</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Settings</Dropdown.Item>
+      </Dropdown.Menu>
+        </Dropdown>
         </div>
         <div className="Nameofuser">
         <h1>Hi Erez,</h1>
