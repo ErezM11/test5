5131a4855917001e314220bf1085317a2f5d387c
erez <erezmantzoor11@gmail.com>
2025-03-23 16:02:55 +0200

    erez ha meleh

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Graph from "./component/Graph";
 import TableUsers from "./component/TableUsers";
 import NavBar from "./component/NavBar";
 
+import MostSales from "./component/MostSales";
 export default function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Graph></Graph>
       <TableUsers></TableUsers>
+      <MostSales/>
+
     </div>
   );
 }
