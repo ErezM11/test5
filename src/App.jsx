780c16f24ecdc2f5b218e99466512d99b64efd9a
erez <erezmantzoor11@gmail.com>
2025-03-23 16:02:55 +0200

    erez ha meleh

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import TableUsers from "./component/TableUsers";
 import NavBar from "./component/NavBar";
 
 import MostSales from "./component/MostSales";
-
 export default function App() {
   return (
     <div>
